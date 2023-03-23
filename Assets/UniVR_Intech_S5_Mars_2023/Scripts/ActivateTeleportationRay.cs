aaf044179954c0cae86aa908b4571d3905cd1cfe
ExtraPerry <pierreextraperry@gmail.com>
2023-03-23 19:45:48 +0100

    Added - Menu UI example & Stargate Props. & Updtaed .gitignore for TextMesh Pro.

--- Assets/UniVR_Intech_S5_Mars_2023/Scripts/ActivateTeleportationRay.cs
+++ Assets/UniVR_Intech_S5_Mars_2023/Scripts/ActivateTeleportationRay.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 
 public class ActivateTeleportationRay : MonoBehaviour
@@ -14,10 +15,22 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty leftCancel;
     public InputActionProperty rightCancel;
 
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rgihtRay;
+
     // Update is called once per frame
     void Update()
     {
-        leftTeleportation.SetActive(leftCancel.action.ReadValue<float>() < 0.15f && leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() < 0.15f && rightActivate.action.ReadValue<float>() > 0.1f);
+        bool isLeftTriggerPulled = leftActivate.action.ReadValue<float>() > 0.1f;
+        bool isRightTriggerPulled = rightActivate.action.ReadValue<float>() > 0.1f;
+
+        bool isLeftGripReleased = leftCancel.action.ReadValue<float>() < 0.15f;
+        bool isRightGripReleased = rightCancel.action.ReadValue<float>() < 0.15f;
+
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        bool isRightRayHovering = leftRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool lrightValid);
+
+        leftTeleportation.SetActive((isLeftTriggerPulled && isLeftGripReleased) && (!isLeftRayHovering));
+        rightTeleportation.SetActive((isRightTriggerPulled && isRightGripReleased) && (!isRightRayHovering));
     }
 }

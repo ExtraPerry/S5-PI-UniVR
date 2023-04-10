84d65c256f89bdbab895690fcf44d9fe7c08831b
ExtraPerry <pierreextraperry@gmail.com>
2023-04-10 20:41:10 +0200

    Made simple menu with half working actions.

---
+++ Assets/UniVR_Intech_S5_Mars_2023/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject stargate;
+    public Rigidbody playerRigidbody;
+    public Transform worldSpawn;
+
+    // Dev utility to force the gate open for the next level.
+    public void forceStartGate()
+    {
+        Debug.Log("Force started the Stargate for next level.");
+    }
+
+    // Dev utility to force the player to respawn (to be used in-case something goes wrong).
+    public void forceRespawnPlayer()
+    {
+        playerRigidbody.position = worldSpawn.position;
+        Debug.Log("Force respawned the player back to World Spawn .");
+    }
+
+    // Button used to exit the game. Note : does not work in play test mode. It only works on a deployed application.
+    public void exitApplication()
+    {
+        Application.Quit();
+        Debug.Log("Exiting the Game.");
+    }
+}

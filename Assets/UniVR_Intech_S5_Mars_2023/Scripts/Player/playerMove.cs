779f9a31e0deeb813f3b5ceef114739b44d1bc9e
DarelTheR <64098859+DarelTheR@users.noreply.github.com>
2023-04-27 01:43:15 +0200

    Player Move script

--- Assets/UniVR_Intech_S5_Mars_2023/Scripts/Player/playerMove.cs
+++ Assets/UniVR_Intech_S5_Mars_2023/Scripts/Player/playerMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
+    public int speed = 3;
+    public int valForce = 300;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class playerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * v * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * h * Time.deltaTime * speed);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * valForce);
+        }
         
     }
 }
